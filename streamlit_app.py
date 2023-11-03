f44c7e876ded8e73380ce86fb5036f657f6d7a7a
ogre0382 <115123784+ogre0382@users.noreply.github.com>
2023-11-03 21:55:48 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,14 @@
 import streamlit as st
+import sqlite3
 from PIL import Image
+
+conn = sqlite3.connect('./static/TEST.db')
+
+cur = conn.cursor()  # カーソルを作成
+cur.execute('CREATE TABLE persons(id INTEGER PRIMARY KEY AUTOINCREMENT, name STRING)')  # tableを作成する指示
+
+conn.commit()  # commit()した時点でDBファイルが更新されます
+conn.close()
+
 st.image('./static/logo2.png')
 st.image('./static/gameset2.png')
