8fe996f0cc8ad36f4edf64266e73c988b9b193d9
ogre0382 <115123784+ogre0382@users.noreply.github.com>
2023-11-03 21:45:35 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from PIL import Image
-st.image(Image.open('./static/logo.png'))
-st.image(Image.open('./static/logo2.png'))
-st.image(Image.open('./static/gameset.png'))
-st.image(Image.open('./static/gameset2.png'))
+st.image('./data/logo.png')
+st.image('./data/logo2.png')
+st.image('./data/gameset.png')
+st.image('./data/gameset2.png')
