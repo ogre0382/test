85228440f7ad3fb51a65363a93bcae6bab151593
ogre0382 <115123784+ogre0382@users.noreply.github.com>
2023-11-03 21:38:24 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,22 +1,6 @@
 import streamlit as st
 from PIL import Image
-
-#where_req = ('first_color = True',)
-#print(' AND '.join(where_req))
-
-st.markdown("./app/static/logo.png")
-st.markdown("""
-    <style>
-    .my-text {
-        color: white;
-        font-size: 24px;
-        background-color: #008080;
-        padding: 10px;
-        border-radius: 10px;
-    }
-    </style>
-    <p class='my-text'>Hello World!<br>
-    <img src="./app/static/logo.png" height="150" width="150" style="vertical-align:middle;"></p>
-    """, unsafe_allow_html=True)
-image = Image.open('./app/static/logo.png')
-st.image(image)
+st.image('./static/logo.png')
+st.image('./static/logo2.png')
+st.image('./static/gameset.png')
+st.image('./static/gameset2.png')
