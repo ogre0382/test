de9057303678ed5a35a1a073ae23d2ada904c762
ogre0382 <115123784+ogre0382@users.noreply.github.com>
2023-11-04 11:17:31 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,66 @@
 import streamlit as st
-import sqlite3
+from google.oauth2 import service_account
 from PIL import Image
 
-conn = sqlite3.connect('./static/TEST.db')
+import io
+import os
 
-cur = conn.cursor()  # カーソルを作成
-cur.execute('CREATE TABLE IF NOT EXISTS persons(id INTEGER PRIMARY KEY AUTOINCREMENT, name STRING)')  # tableを作成する指示
+# Imports the Google Cloud client library
+from google.cloud import vision
 
-conn.commit()  # commit()した時点でDBファイルが更新されます
-conn.close()
+from paddleocr import PaddleOCR
 
-st.image('./static/logo2.png')
-st.image('./static/gameset2.png')
+# Google CloudのCloud Vision APIで画像から日本語の文字抽出をしてみた
+# https://dev.classmethod.jp/articles/google-cloud_vision-api/
+def test_gcv():
+    # Instantiates a client
+    credentials = service_account.Credentials.from_service_account_info(
+            st.secrets["gcp_service_account"]
+        )
+    client = vision.ImageAnnotatorClient(credentials=credentials)
+
+    # The name of the image file to annotate
+    file_name = os.path.abspath('./images/4565.0.jpg')
+
+    # Loads the image into memory
+    with io.open(file_name, 'rb') as image_file:
+        content = image_file.read()
+
+    image = vision.Image(content=content)
+
+    # Performs label detection on the image file
+    response =  client.document_text_detection(
+            image=image,
+            image_context={'language_hints': ['ja']}
+        )
+
+    # レスポンスからテキストデータを抽出
+    output_text = ''
+    for page in response.full_text_annotation.pages:
+        for block in page.blocks:
+            for paragraph in block.paragraphs:
+                for word in paragraph.words:
+                    output_text += ''.join([
+                        symbol.text for symbol in word.symbols
+                    ])
+                output_text += '\n'
+    print(output_text)
+
+def test_paddleocr():
+
+    ocr = PaddleOCR(use_angle_cls=True, use_gpu=False, lang="japan")
+
+    img_path = './images/4565.0.jpg'
+    result = ocr.ocr(img_path, cls=True)[0]
+
+    for line in result:
+        print(line)
+
+#where_req = ('first_color = True',)
+#print(' AND '.join(where_req))
+
+# image = Image.open('./app/static/logo2.png')
+#st.image(image)
+
+test_gcv()
+test_paddleocr()
